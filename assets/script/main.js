abbc3f8414b958d795ac1d14d0d462aa3b45cf2b
vzhz <vehanus@gmail.com>
2018-04-26 17:53:41 -0400

    Will now feed last 6 photos from instagram, closes #5

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,6 +4,7 @@ var feed = new Instafeed({
 		// clientId: 'b8f69e8085334c6791b69b1c4d2d8d10',
 		accessToken: '6265799545.1677ed0.01b7db90bbf54451bf38b65d7eca25ab',
 		target: 'instagram',
+		limit: 6,
 		resolution: 'standard_resolution',
 		after: function() {
 			var el = document.getElementById('instagram');
